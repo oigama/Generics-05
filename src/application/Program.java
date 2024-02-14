379f8fc442302be833b29715b59bf8f89b9c9f5d
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-14 09:58:50 -0300

    Test

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,15 @@ import entities.Client;
 public class Program {
     public static void main(String[] args) {
         Client c1 = new Client("Matheus", "mathemovic.mg@gmail.com");
-        Client c2 = new Client("Vanessa", "vanessprazeres@gmail.com");
+        Client c2 = new Client("Matheus", "mathemovic.mg@gmail.com");
+
+        String s1 = "Test";
+        String s2 = "Test";
 
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
     }
 }
