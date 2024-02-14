35faeca714d5940ebe2df0dc4dba4dd0fa205676
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-14 08:54:38 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+        String a = "Matheus";
+        String b = "Matheus";
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
